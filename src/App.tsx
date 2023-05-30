fedac908b16e9ed372b603a6674ac1aa77166a90
JYuan <13826506407@139.com>
2023-05-30 23:15:29 +0800

    运行cesium成功

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+//@ts-ignore;
+import * as Cesium from 'cesium/Cesium';
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const viewer = new Cesium.Viewer("cesiumContainer");
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="cesiumContainer" />
     </div>
   );
 }
